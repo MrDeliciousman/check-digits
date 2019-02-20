942f19758ff305fabaddc72b716ddd6f31cf704a
Thomas Herrera <rayherrera90@outlook.com>
2019-02-19 19:08:16 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Check.java
@@ -0,0 +1,28 @@
+package edu.cnm.deepdive;
+
+import java.util.Scanner;
+
+public class Check {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int pass = scanner.nextInt();
+    Check.digitSum(pass);
+    System.out.println(digitSum(pass));
+    System.out.println(digitSum(pass) / 9);
+    System.out.println(digitSum(pass));
+    System.out.println(digitSum(pass) / 9);
+  }
+
+  public static int digitSum(int pass) {
+    String str;
+    str = Integer.toString(pass);
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+      count += str.charAt(i) - 48;
+    }
+    return count;
+  }
+
+}
